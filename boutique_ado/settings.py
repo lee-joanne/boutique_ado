cb95108047f002760c165f1ab7f8f8b00c28d63f
Joanne <lee.joanne54@gmail.com>
2023-06-23 20:29:31 +0000

    Complete order success page

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 import stripe
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -163,5 +165,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'usd'
-STRIPE_PUBLIC_KEY = os.getenv("STRIPE_PUBLIC_KEY", '')
-STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY", '')
+STRIPE_PUBLIC_KEY = os.environ.get("STRIPE_PUBLIC_KEY")
+STRIPE_SECRET_KEY = os.environ.get("STRIPE_SECRET_KEY")
