8cb52ae76c918255dd6d98b7160cc852d0c607df
Joanne <lee.joanne54@gmail.com>
2023-07-05 03:10:55 +0000

    Adjust aws region

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -140,7 +140,7 @@ else:
 if 'USE_AWS' in os.environ:
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'ba-ci'
-    AWS_S3_REGION_NAME = 'us-east-1'
+    AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
