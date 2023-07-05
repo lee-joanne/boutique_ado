3e7e7f1cc2313dcd99782fd972a1b326bb94f01d
Joanne <lee.joanne54@gmail.com>
2023-07-05 03:23:09 +0000

    Add AWS_S3_OBJECT_PARAMETERS

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -138,13 +138,18 @@ else:
     
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'ba-ci'
     AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-    AWS_DEFAULT_ACL = None
 
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
